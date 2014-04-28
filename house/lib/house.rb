fd898552d91b3dc33e8296bf95e73547e97602c4
James Tharpe <jamestharpe@careerbuilder.com>
2014-04-28 11:27:17 -0400

    Reduces duplication

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,22 +1,25 @@
 class House
-  @@poem = ["This is the house that Jack built.\n",
-           "This is the malt that lay in the house that Jack built.\n",
-           "This is the rat that ate the malt that lay in the house that Jack built.\n",
-           "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-           "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-           "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-           "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-           "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-           "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-           "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-           "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-           "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"]
+
+  Line_start, Line_end = 'This is the ', ".\n"
+
+  Parts = ['house that Jack built',
+           'malt that lay in ',
+           'rat that ate ',
+           'cat that killed ',
+           'dog that worried ',
+           'cow with the crumpled horn that tossed ',
+           'maiden all forlorn that milked ',
+           'man all tattered and torn that kissed ',
+           'priest all shaven and shorn that married ',
+           'rooster that crowed in the morn that woke ',
+           'farmer sowing his corn that kept ',
+           'horse and the hound and the horn that belonged to ']
 
   def line(num)
-    @@poem[num - 1]
+    Line_start + Parts.slice(0, num).reverse.join('the ') + Line_end
   end
 
   def recite
-    return @@poem.join "\n"
+    (0..Parts.length-1).map { |i| line i+1 }.join "\n"
   end
 end
